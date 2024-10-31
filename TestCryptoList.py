7bf18f4008e698012f9905d55802610034a98c67
hyodong123 <162409720+hyodong123@users.noreply.github.com>
2024-10-31 11:55:52 +0900

    TestCryptoList.py

--- TestCryptoList.py
+++ TestCryptoList.py
@@ -557,7 +557,7 @@ def footer():
         """
         <footer style='text-align: center; margin-top: 50px;'>
             <hr>
-            <p>Copyright Team 비트지니어스 - 비트알고 프로젝트 © 2024. All Rights Reserved.</p> 
+            <p>Copyright Team 비트지니어스 - 비트알고 프로젝트 &copy; 2024. All Rights Reserved.</p>
         </footer>
         """,
         unsafe_allow_html=True
